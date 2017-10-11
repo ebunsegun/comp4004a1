d88a9f65e5faa4c1af00054d91f6e6b701211262
ebunsegun <segunebunoluwa@gmail.com>
2017-10-11 12:52:01 -0400

    Added acceptance test for user simply borrowing an available book (without outstanding fees)

---
+++ test/acceptance_tests/BorrowLoanCopyTest.java
@@ -0,0 +1,43 @@
+package acceptance_tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import server.logic.handler.OutputHandler;
+import server.logic.handler.model.Output;
+import server.logic.tables.ItemTable;
+import server.logic.tables.LoanTable;
+import server.logic.tables.TitleTable;
+import server.logic.tables.UserTable;
+
+public class BorrowLoanCopyTest {
+	OutputHandler outputHandler = new OutputHandler();
+    public static final int USER = 3;
+    String newUsername = "";
+    String ISBN = "";
+    String borrowInformation = "";
+    
+    @Before
+    public void setUp() {
+    	//Create and add a new user to the system since new users do not have fees or existing loans
+    	newUsername = "Umar@carleton.ca";
+    	UserTable.getInstance().createuser(newUsername, "Umar");
+    	//Add a new title and item to the system for this user to borrow
+    	ISBN = "9123456789125";
+    	TitleTable.getInstance().createtitle(ISBN, "Seasons of Life");
+    	ItemTable.getInstance().createitem(ISBN);
+    	borrowInformation = newUsername + "," + ISBN + ",1";
+    }
+
+    @Test
+    public void borrowWithoutExistingFeeTest() {
+    	Output output = outputHandler.borrow(borrowInformation);
+    	assertEquals("Success!", output.getOutput());
+    	assertEquals(USER, output.getState()); 
+    	assertTrue(LoanTable.getInstance().lookup(ISBN, "1"));
+    }
+
+}
