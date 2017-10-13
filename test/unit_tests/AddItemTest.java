581ed3d8570e309185537b055b8204164a271fc7
ebunsegun <segunebunoluwa@gmail.com>
2017-10-13 00:48:22 -0400

    Added unit test for adding item use case

---
+++ test/unit_tests/AddItemTest.java
@@ -0,0 +1,23 @@
+package unit_tests;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import server.logic.model.Item;
+import server.logic.model.Title;
+import server.logic.tables.ItemTable;
+import server.logic.tables.TitleTable;
+
+public class AddItemTest {
+
+	@Test
+	public void test() {
+		int itemTableSize = ItemTable.getInstance().getItemTable().size();
+		Item item = new Item(itemTableSize, "9009858946734", "1");
+		ItemTable.getInstance().getItemTable().add(item);
+		assertEquals(itemTableSize + 1, ItemTable.getInstance().getItemTable().size());
+		assertEquals("9009858946734", ItemTable.getInstance().getItemTable().get(itemTableSize).getISBN());
+		assertEquals("1", ItemTable.getInstance().getItemTable().get(itemTableSize).getCopynumber());
+	}
+}
